4475a67ef066c6c77d638778915b7a5033a196c5
Saaduddin <saaduddin2325@gmail.com>
2023-02-14 18:53:05 -0600

    New Upload

---
+++ initials.py
@@ -0,0 +1,9 @@
+# Mohammad Saaduddin
+# I am cool
+print("M   M U   U")
+print("MM MM U   U")
+print("MM MM U   U")
+print("M M M U   U")
+print("M   M U   U")
+print("M   M U   U")
+print("M   M  UUU ")
